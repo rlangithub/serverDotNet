d22bf8114d1ec6cde9943c63169b704fe17aaa99
rlanggithub <r0556721183@gmail.com>
2024-02-07 22:14:56 +0200

    mytext

---
+++ server/MiddleWares/Middleware.cs
@@ -0,0 +1,24 @@
+﻿
+using Serilog;
+
+namespace Server.MiddleWares
+{
+    public class Middleware
+    {
+            private readonly RequestDelegate _next;
+
+            public Middleware(RequestDelegate next)
+            {
+                _next = next;
+            }
+
+            public async Task InvokeAsync(HttpContext httpContext)
+            {
+                var myAction = httpContext.GetRouteData().Values["action"]?.ToString();
+                Log.Information("action:" + " " + myAction);
+                Log.Information("from new middleware");
+                await _next(httpContext);
+            }
+        }
+    }
+
